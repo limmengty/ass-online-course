feeb163baef0f8ae8184afbaa4f6287dfc6560c0
Mengty <limmengti@gmail.com>
2024-06-09 19:54:37 +0700

    add navbar and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  variants: {
+    extends: {
+      visibility: ["group-hover"],
+    },
+  },
+  screen: {
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+  },
+  container: {
+    center: true,
+    padding: "1rem",
+  },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      fontSize: {
+        sm: [
+          "14px",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.01rem",
+            fontWeight: "400",
+          },
+        ],
+        base: [
+          "16px",
+          {
+            lineHeight: "2rem",
+            letterSpacing: "-0.01rem",
+            fontWeight: "500",
+          },
+        ],
+        lg: ["20px", "28px"],
+        xl: ["24px", "32px"],
+      },
+      colors: {
+        gray: "#e7ecef",
+        blue: {
+          dark: "#274c77",
+          light: "#a3cef1",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
